052412ba85583ade747717610feb4d9f1e7b0c83
aldlflcl <64316701+aldlflcl@users.noreply.github.com>
2022-07-06 03:22:24 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        int max = Integer.MIN_VALUE;
+        int idx = -1;
+        for (int i = 0; i < 9; i++) {
+           arr[i] = Integer.valueOf(br.readLine());
+           if (max < arr[i]) {
+               max = arr[i];
+               idx = i;
+           }
+        }
+        System.out.printf("%d%n%d", max, idx + 1);
+    }
+}
