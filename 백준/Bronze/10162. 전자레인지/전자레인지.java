07a92634698eb08b027a14085eba39b6c859b986
aldlflcl <64316701+aldlflcl@users.noreply.github.com>
2022-07-10 15:29:53 +0900

    [Bronze IV] Title: 전자레인지, Time: 84 ms, Memory: 11744 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.valueOf(br.readLine());
+        int ten = 0;
+        int minute = 0;
+        int fiveM = 0;
+        if (t >= 300) {
+            fiveM += t / 300;
+            t = t % 300;
+        }
+        if (t >= 60) {
+            minute += t / 60;
+            t = t % 60;
+        }
+        if (t >= 10) {
+            ten += t / 10;
+            t = t % 10;
+        }
+        if (t != 0) System.out.print(-1);
+        else System.out.printf("%d %d %d", fiveM, minute, ten);
+    }
+}
