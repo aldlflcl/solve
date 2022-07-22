d3470175e21d6972d5e6869538d7c61d83fb19ff
aldlflcl <64316701+aldlflcl@users.noreply.github.com>
2022-07-22 18:55:28 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1264 ms, Memory: 337604 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -5,52 +5,72 @@ import java.util.*;
 
 public class Main {
 
+    static class Graph {
+
+        private final List<List<Integer>> list = new ArrayList<>();
+        private final int[] d;
+
+        public Graph(int initialSize) {
+            for (int i = 0; i <= initialSize; i++) {
+                list.add(new ArrayList<>());
+            }
+            d = new int[initialSize + 1];
+            Arrays.fill(d, -1);
+        }
+
+        public void put(int x, int y) {
+            list.get(x).add(y);
+        }
+
+        public int[] find(int start, int k) {
+            d[start] = 0;
+            List<Integer> result = new ArrayList<>();
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(start);
+
+            while (!q.isEmpty()) {
+                start = q.poll();
+                for (int i = 0; i < list.get(start).size(); i++) {
+                    int dest = list.get(start).get(i);
+                    if (d[dest] == -1) {
+                        d[dest] = d[start] + 1;
+                        q.offer(dest);
+                        if (d[dest] == k) {
+                            result.add(dest);
+                        }
+                    }
+                }
+            }
+            return d;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<List<Integer>> list = new ArrayList<>();
+        int n = Integer.valueOf(st.nextToken()); // 도시의 개수
+        int m = Integer.valueOf(st.nextToken()); // 도로의 개수
+        int k = Integer.valueOf(st.nextToken()); // 거리 정보
+        int x = Integer.valueOf(st.nextToken()); // 출발 도시의 번호
 
-        int n = Integer.parseInt(st.nextToken()); // 도시의 개수
-        int m = Integer.parseInt(st.nextToken()); // 도로의 개수
-        int k = Integer.parseInt(st.nextToken()); // 거리 정보
-        int x = Integer.parseInt(st.nextToken()); // 출발 도시의 번호
-        int[] d = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-            d[i] = -1;
-        }
+        Graph graph = new Graph(n);
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            list.get(Integer.parseInt(st.nextToken())).add(Integer.parseInt(st.nextToken()));
+            int start = Integer.valueOf(st.nextToken());
+            int dest = Integer.valueOf(st.nextToken());
+            graph.put(start, dest);
         }
 
-        bfs(list, x, k, d);
-    }
-
-    public static void bfs(List<List<Integer>> list, int x, int k, int[] d) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(x);
-        d[x] = 0;
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            for (int i = 0; i < list.get(now).size(); i++) {
-                int next = list.get(now).get(i);
-                if (d[next] == -1) {
-                    d[next] = d[now] + 1;
-                    q.offer(next);
-                }
-            }
-        }
+        int[] result = graph.find(x, k);
 
         boolean flag = false;
-
-        for (int i = 0; i < d.length; i++) {
-            if (d[i] == k) {
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == k) {
                 flag = true;
                 System.out.println(i);
             }
         }
-
-        if (!flag) System.out.print(-1);
+        if (!flag) System.out.println(-1);
     }
 }
